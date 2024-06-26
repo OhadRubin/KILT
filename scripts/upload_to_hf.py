33fa9f2b1959135ccaeab61364f8a54dca70e95b
Your Name <you@example.com>
2024-06-26 08:01:39 +0000

    ..

--- scripts/upload_to_hf.py
+++ scripts/upload_to_hf.py
@@ -12,16 +12,16 @@ from kilt.kilt_utils import load_data
 
 WIKI_INDEX_PATH = "/dev/shm/lucane_indexes/kilt_w100_sharded"
 KILT_DATA_PREFIX = "/dev/shm/kilt_data/data"
-
+datasets.disable_caching()
 def decode_doc(doc):
-    return json.loads(doc.raw())["contents"]
+    return json.loads(doc.raw())
 
 def search_question(batch: Dict, searcher: LuceneSearcher) -> Dict:
     qids = batch["qid"]
-    hits = searcher.batch_search(batch["question"], qids, threads=300, k=100)
+    hits = searcher.batch_search(batch["question"], qids, threads=50, k=100)
     ctxs_per_doc = [[hit.docid for hit in hits[qid]] for qid in qids]
     ctxs = sum(ctxs_per_doc, [])
-    doc_res = searcher.batch_doc(ctxs, threads=300)
+    doc_res = searcher.batch_doc(ctxs, threads=50)
     docs_raw = [[decode_doc(doc_res[x]) for x in doc_hits] for doc_hits in ctxs_per_doc]
     batch["ctxs"] = docs_raw
     return batch
@@ -46,7 +46,8 @@ def get_paths(prefix: str) -> List[Dict]:
     return df.to_dict("records")
 
 from itertools import islice
-def process_dataset(file_path: str, searcher: LuceneSearcher, limit_samples=None) -> datasets.Dataset:
+
+def process_dataset(file_path: str, searcher: LuceneSearcher, cache_file_name, limit_samples=None) -> datasets.Dataset:
 
     def gen():
         for x in islice(load_data(file_path), limit_samples):
@@ -61,32 +62,41 @@ def process_dataset(file_path: str, searcher: LuceneSearcher, limit_samples=None
         batch_size=300,
         batched=True,
         fn_kwargs={"searcher": searcher},
-        # cache_file_name=f"/dev/shm/tmp/{name}_{split}.arrow"
+        cache_file_name=cache_file_name
     )
     
     return mapped_dataset
-
+import shutil
+import os
 def generate_dataset(record: Dict, searcher: LuceneSearcher, limit_samples=None) -> datasets.DatasetDict:
+    os.makedirs("/dev/shm/tmp", exist_ok=True)
     dataset_dict = {}
+    name = record.pop("name")
     for split in record.keys():
-        if split=="name":
-            continue
         if record.get(split):
-            print(record[split])
-            dataset_dict[split] = process_dataset(record[split], searcher, limit_samples)
+            print(name)
+            cache_file_name = f"/dev/shm/tmp/{name}_{split}.arrow"
+            dataset_dict[split] = process_dataset(record[split], searcher, cache_file_name, limit_samples)
+            os.remove(cache_file_name)
+    dataset_dict = datasets.DatasetDict(dataset_dict)
+    dataset_dict.push_to_hub(f"{name}_bm25_top100_kilt", token=os.environ["HF_TOKEN"])
+    shutil.rmtree("/dev/shm/tmp")
     
-    return datasets.DatasetDict(dataset_dict)
+    # return 
 
 def main():
-    searcher = LuceneSearcher(WIKI_INDEX_PATH)
     records = get_paths(KILT_DATA_PREFIX)
-    
+    searcher = LuceneSearcher(WIKI_INDEX_PATH)
     for record in records:
         dataset_name = record['name']
+        if dataset_name not in ["cweb"]:
+            continue
         print(f"Processing dataset: {dataset_name}")
-        dataset_dict = generate_dataset(record, searcher)
-        if dataset_dict:
-            dataset_dict.push_to_hub(f"{dataset_name}_bm25_top100_kilt", token=os.environ["HF_TOKEN"])
+        generate_dataset(record, searcher,limit_samples=2000)
+            
+        # try:
+        # except Exception as e:
+        #     pass
         # Save to disk or push to HuggingFace Hub
         # cache_dir = f"/dev/shm/datasets/{dataset_name}_bm25_top100"
         # dataset_dict.save_to_disk(cache_dir)
